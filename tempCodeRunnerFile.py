b9ea50716d0b2b4ccf81786225dd8925c07b5a50
puppy-lol <77181404+puppy-lol@users.noreply.github.com>
2022-02-06 14:40:17 -0600

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+maps_dictionary = { "India" : india_map , 
+#                     "America" : america_map ,
+#                     "Australia" : australia_map ,
+#                     "Philippines" : philippines_map,
+#                     "Japan" : japan_map,}
